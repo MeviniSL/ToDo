f999d8ca518f4ae766b0cc1993097ead0cefd8e6
Mevini Silva <silvamevini19@gmail.com>
2025-11-09 15:23:07 +0530

    add edit task function

--- todo_main/views.py
+++ todo_main/views.py
@@ -12,7 +12,12 @@ def home(request):
 
     # show pending tasks
     tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
+
+    completed_tasks = Task.objects.filter(is_completed=True)
     context = {
         'tasks': tasks,
+        'completed_tasks': completed_tasks,
     }
+    
+    
     return render(request, 'home.html', context)
